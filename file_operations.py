0ece4579671f98e7009de2e628fd93ed4556367a
Sam Naveenkumar .V <143938999+samnaveenkumaroff@users.noreply.github.com>
2024-06-19 17:26:50 +0530

    Contains File operations

---
+++ file_operations.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import os
+from openpyxl import load_workbook
+
+class FileOperations:
+    def __init__(self):
+        pass
+
+    def load_sheet_names(self, file_path, combobox):
+        try:
+            workbook = load_workbook(file_path)
+            sheets = workbook.sheetnames
+            combobox['values'] = sheets
+            if sheets:
+                combobox.current(0)
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to load sheets: {str(e)}")
+
+    def view_file(self, file_path, combobox):
+        try:
+            if os.path.exists(file_path):
+                workbook = load_workbook(file_path, read_only=True)
+                sheets = workbook.sheetnames
+                combobox['values'] = sheets
+                if sheets:
+                    combobox.current(0)
+            else:
+                messagebox.showerror("Error", "File not found.")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
